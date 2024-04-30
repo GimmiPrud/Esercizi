3e5566e5ed8997dc843e3ac4ea4a289790a23878
GimmiPrud <prudenzig@gmail.com>
2024-04-30 18:28:24 +0200

    finished ES. 5-6

--- Lezione3/Lezione3.py
+++ Lezione3/Lezione3.py
@@ -282,6 +282,33 @@ elif alien_color == "red":
 else:
     print("good job you killed yellow alien. You just earned 10 points")
     
+# ES. 5-6:
+'''
+Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+• If the person is less than 2 years old, print a message that the person is a baby.
+• If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+• If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+• If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+• If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+• If the person is age 65 or older, print a message that the person is an elder.
+'''
+# Soluzione ES. 5-6:
+
+age: int = int(input("inserisci l'età di una persona: "))
+
+if age < 2:
+    print("that person is a baby")
+elif 4 < age < 13:
+    print("that person is a toddler")
+elif 13 < age < 20:
+    print("that person is a teenager")
+elif 20 < age < 65:
+    print("that person is an adult")
+elif age >= 65:
+    print("that person is an elder")
+    
+# ES. 5-7:
+
 
 
 
