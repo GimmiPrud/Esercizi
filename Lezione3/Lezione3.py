9c4568b42e7d245b788d9ec8066f3b28af2d6f4e
GimmiPrud <prudenzig@gmail.com>
2024-04-30 14:41:44 +0200

    svolgendo esercizi 4-14 & 4-15

--- Lezione3/Lezione3.py
+++ Lezione3/Lezione3.py
@@ -9,7 +9,7 @@ For each pizza, you should have one line of output containing a simple statement
 '''
 # Soluzione ES. 4-1:
 
-favourite_pizza: list = ["Diavola","Margherita","Quattro formaggi"]
+favourite_pizza: list[str] = ["Diavola","Margherita","Quattro formaggi"]
 for pizza in favourite_pizza:
     print(f"pizze preferite:\n{pizza}")
     print(f" mi piace molto la pizza {pizza}")
@@ -26,7 +26,7 @@ You could print a sentence, such as Any of these animals would make a great pet!
 
 # Soluzione ES. 4-2:
 
-animals: list = ["pipistrello","gatto","gufo"]
+animals: list[str] = ["pipistrello","gatto","gufo"]
 for animal in animals:
     print(animal+ ":")
     print(f"il {animal} è molto agile al buio")
@@ -48,9 +48,9 @@ Make a list of the numbers from one to one million, and then use a for loop to p
 (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
 '''
 # Soluzione ES. 4-4:
-
-# for num in range(0,1000001):
-  # print(num)
+if False:
+    for num in range(0,1000001):
+        print(num)
 
 # ES. 4-5:
 '''
@@ -103,7 +103,7 @@ Use a list comprehension to generate a list of the first 10 cubes.
 '''
 # Soluzione ES. 4-9:
 
-cubes: list = [n**3 for n in range(1,11)]
+cubes: list[int] = [n**3 for n in range(1,11)]
 print(cubes)
 
 # ES. 4-10:
@@ -116,6 +116,66 @@ Then use a slice to print three items from the middle of the list.
 • Print the message The last three items in the list are:. Then use a slice to print the last three items in the list.
 '''
 # Soluzione ES. 4-10:
+
+cubes: list[int] = [n**3 for n in range(1,11)]
+print(cubes)
+print(cubes[:3])
+print(cubes[4:7])
+print(cubes[-3:])
+
+# ES. 4-11:
+'''
+Start with your program from Exercise 4-1. Make a copy of the list of pizzas, and call it friend_pizzas.
+Then, do the following:
+• Add a new pizza to the original list.
+• Add a different pizza to the list friend_pizzas.
+• Prove that you have two separate lists.
+Print the message My favorite pizzas are:, and then use a for loop to print the first list.
+Print the message My friend’s favorite pizzas are:, and then use a for loop to print the second list.
+Make sure each new pizza is stored in the appropriate list.
+'''
+# ES. 4-12:
+'''
+All versions of foods.py in this section have avoided using for loops when printing, to save space.
+Choose a version of foods.py, and write two for loops to print each list of foods.
+'''
+# Soluzione ES. 4-11 & 4-12:
+
+favourite_pizza: list = ["Diavola","Margherita","Quattro formaggi"]
+
+Friend_pizzas: list = favourite_pizza.copy()
+
+favourite_pizza.append("Capricciosa")
+Friend_pizzas.append("Boscaiola")
+
+print("My favorite pizzas are:")
+for pizze in favourite_pizza:
+    print(pizze)
+
+print("My friend’s favorite pizzas are:") 
+for p in Friend_pizzas:
+    print(p)
+    
+# ES. 4-14:
+'''
+Look through the original PEP 8 style guide at https://python.org/dev/peps/pep-0008.
+You won’t use much of it now, but it might be interesting to skim through it.
+'''
+# ES. 4-15:
+'''
+Choose three of the programs you’ve written in this chapter and modify each one to comply with PEP 8.
+'''
+# Soluzione ES. 4-14 & 4-15:
+
+
+
+
+    
+
+
+
+
+    
 
 
     
