2c7adc1f03ca18a5c9192b17a161bec2da290caf
GimmiPrud <prudenzig@gmail.com>
2024-04-30 17:22:34 +0200

    finished ES. 5-2

--- Lezione3/Lezione3.py
+++ Lezione3/Lezione3.py
@@ -228,14 +228,27 @@ print(bird != len('horse'))
 
 # Per L'esercizio 5-2 ho creato un file conditional_tests.py sulla cartella Lezione3
 
-# ES. 5-3:
+# ES. 5-3, 5-4, 5-5:
 '''
 Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
 • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
 • Write one version of this program that passes the if test and another that fails.
 (The version that fails will have no output.)
+
+Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+• Write one version of this program that runs the if block and another that runs the else block.
+
+into an if-elif-else chain.
+• If the alien is green, print a message that the player earned 5 points.
+• If the alien is yellow, print a message that the player earned 10 points.
+• If the alien is red, print a message that the player earned 15 points.
+• Write three versions of this program, making sure each message is printed for the appropriate color alien.
 '''
-# Soluzione ES. 5-3:
+# Soluzione ES. 5-3, 5-4, 5-5:
+
+
 
 
 
--- Lezione3/conditional_tests.py
+++ Lezione3/conditional_tests.py
@@ -16,3 +16,32 @@ than, greater than or equal to, and less than or equal to
 
 # Soluzione ES. 5-2:
 
+if True:
+    food: str = "Lasagna"
+    car: str = "Renault"
+    thing: str = "hammer"
+    num1: int = 19
+    num2: int = 123
+    cars: list[str] = ["Audi","Bmw","Panda","Alfa Romeo","Cupra","Lotus","Ferrari",]
+    
+    print("Is thing == 'Panda'? I predict False")
+    print(thing == "Panda")
+    print("\nIs thing == thing in uppercase ? I predict False")
+    print(thing == thing.upper())
+    print("\nIs num1 > num2 ? I predict False")
+    print(num1 > num2)
+    print("\nIs num1*6+9 <= num2 ? I predict True")
+    print(num1*6+9 <= num2)
+    print("\nIs 'Cupra' in cars ? I predict True")
+    print("Cupra" in cars)
+    print("\nIs car in car or 'Bmw' in car ? I predict True")
+    print(car in car or 'Bmw' in car)
+    print("\nIs food == 'Lasagna' and len(food) != len(car) ? I predict False")
+    print( food == 'Lasagna' and len(food) != len(car))
+    print("\nIs 'hammer' not in cars ? I predict True")
+    print("hammer" not in cars)
+    
+    
+    
+    
+
