71d0a11f15d13b595ebb9c089c1b37c03396f624
GimmiPrud <prudenzig@gmail.com>
2024-05-13 13:12:58 +0200

    svolgimento miniprogetto zoo.py

--- Giammarco_Prudenzi/Zoo.py
+++ Giammarco_Prudenzi/Zoo.py
@@ -14,3 +14,27 @@ Funzionalità:
 '''
 # Svolgimento Progetto:
 
+class Zoo:
+    def __init__(self, fences: str, zoo_keepers: str):
+        self.fences = fences
+        self.zoo_keepers = zoo_keepers
+    
+class Animal:
+    def __init__(self, name: str, species: str, age: int, height: float, width: float, preferred_habitat: str, health: float = round(100 * (1 / age), 3)):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.height = height
+        self.width = width
+        self.preferred_habitat = preferred_habitat
+        self.health = health
+        
+class Fence:
+    def __init__(self, area: float, temperature: float, habitat: str):
+        self.area = area
+        self.temperature = temperature
+        self.habitat = habitat
+        
+class ZooKeeper:
+    def __init__(self, name: str, surname: str, id: int):
+        pass
