6108f2ce77e3edc4ef748dac5220b074d32d86af
GimmiPrud <prudenzig@gmail.com>
2024-05-16 11:08:13 +0200

    update Lezione9

--- Lezione9/Lezione9.py
+++ Lezione9/Lezione9.py
@@ -5,3 +5,12 @@
 def nome_funzione(arg_1: int, **kwarg): # si utilizzano per passare qualsiasi argomento (parametri arbitrari) e creare un dizionario 
 
     alpha: float = kwarg["alpha"] # permette di creare altri argomenti senza aggiungerli nella funzione 
+
+def complex_statistical_function(x, distribution_type, **kwargs):
+    if distribution_type == "geometric":
+
+        p: float = kwargs["p"]
+    
+    elif distribution_type == "poisson":
+
+        lambda_1: float = kwargs["lambda_1"] 
