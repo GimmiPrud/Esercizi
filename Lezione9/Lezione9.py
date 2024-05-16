f9a54b8833833df2b2fbf5cd0036617e12a89c80
GimmiPrud <prudenzig@gmail.com>
2024-05-16 10:49:10 +0200

    Lezione 9

---
+++ Lezione9/Lezione9.py
@@ -0,0 +1,7 @@
+# Giammarco Prudenzi 
+
+# Ripasso classi ed introduzione delle keyword args (kwargs)
+
+def nome_funzione(arg_1: int, **kwarg): # si utilizzano per passare qualsiasi argomento (parametri arbitrari) e creare un dizionario 
+
+    alpha: float = kwarg["alpha"] # permette di creare altri argomenti senza aggiungerli nella funzione 
