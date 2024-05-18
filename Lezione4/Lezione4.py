5dbf7927148470b324b00a818af50896f1448634
GimmiPrud <prudenzig@gmail.com>
2024-05-18 19:13:18 +0200

    ES 2 esercizi opzionali lezione4

--- Lezione4/Lezione4.py
+++ Lezione4/Lezione4.py
@@ -344,6 +344,16 @@ if False:
     
     for student in list_student:
         print(student)
+        
+# ES. 2:
+'''
+Create a function that generates a random number within a range specified by the user.
+Prompt the user to guess the number within a specified maximum number of attempts.
+Provide feedback to the user after each guess, indicating whether their guess is too high, too low, or correct.
+Terminate the loop when the user guesses the number correctly or reaches the maximum number of attempts.
+'''
+# Svolgimento ES. 2:
+
     
 
             
