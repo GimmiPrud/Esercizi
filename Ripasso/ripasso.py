99f388349237602231d1e8cbd5fc4f3443a63121
GimmiPrud <prudenzig@gmail.com>
2024-05-31 14:03:39 +0200

    ...

--- Ripasso/ripasso.py
+++ Ripasso/ripasso.py
@@ -4,5 +4,7 @@ class calc:
     def __init__(self, a, b) -> None:
         self.a = a
         self.b = b
-        
+    
+    def get_sum(self):
+        return self.a + self.b
         
--- Tests.py/testing.py
+++ Tests.py/testing.py
@@ -1,6 +1,6 @@
 
 import unittest
-from..Ripasso.ripasso import calc # questo metodo di import si chiama import relativo 
+from Ripasso.ripasso import calc # questo metodo di import si chiama import relativo 
 # import relativo sintassi = "from..nome_directori.nome_file import nome funzione o classe  "
 
 class TestCalculations(unittest.TestCase):
@@ -17,3 +17,5 @@ class TestCalculations(unittest.TestCase):
 
         self.assertEqual(self.calculations.get_sum(), 7, "the sum is wrong")
 
+if __name__== "__main__":
+    unittest.main()
