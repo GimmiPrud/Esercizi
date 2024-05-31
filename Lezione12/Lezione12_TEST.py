6bf98453ae4705ebbfaf16413fc8c46c5431dd85
GimmiPrud <prudenzig@gmail.com>
2024-05-31 11:11:10 +0200

    ...

--- Lezione12/Lezione12_TEST.py
+++ Lezione12/Lezione12_TEST.py
@@ -4,9 +4,9 @@
 i: int = 0
 test_value: int = 0
 
-assert i == 0  # assert viene utilizzato per verificare che una condizione sia True o False (test)
+assert i == 0  # assert viene utilizzato per verificare una condizione  (test)
 # inltre come il metodo raise anche assert può mandare un messaggio perzonalizzato di errore
-
+# sintassi assert
 # Esempi:
 assert i == test_value, f" Error, the value i must be {test_value} instead is {i}"
 
@@ -19,3 +19,6 @@ test_value: int = 7
 assert i == test_value, f" Error, the value i must be {test_value} instead is {i}"
 
 # ci sono diversi tipi di assert che si trovano importando la libreria unittest{}
+
+# la funzine all() di python controlla se tutto quello che è all'interno di un object sia True 
+# se tutto è true il risultato sarà true, se anche una sola condizione è false il risultato sarà false (tipo un and )
