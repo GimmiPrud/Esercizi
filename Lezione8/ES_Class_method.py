f638e426b583b301f1b65eab676be2c493b689de
GimmiPrud <prudenzig@gmail.com>
2024-06-10 11:40:09 +0200

    ...

--- Lezione8/ES_Class_method.py
+++ Lezione8/ES_Class_method.py
@@ -1,2 +1,10 @@
 
 # Giammarco Prudenzi
+# Esercizi su classi astratte, class methods e static methods
+
+# ES.1: Creating an Abstract Class with Abstract Methods:
+'''
+Create an abstract class Shape with an abstract method area and another abstract method perimeter.
+Then, create two subclasses Circle and Rectangle that implement the area and perimeter methods.
+'''
+# Svolgimento ES. 1:
