fecd947775e66eb0cb72de43a576b7ed50a7f663
GimmiPrud <prudenzig@gmail.com>
2024-06-12 11:13:10 +0200

    ..

--- Lezione15/File_context_lettura_scrittura.py
+++ Lezione15/File_context_lettura_scrittura.py
@@ -1,9 +1,11 @@
 
 # Giammarco Prudenzi 
 
-reader = print(open("Lezione15/esempio.txt"))      # ci sono diversi encoding(UTF8, UTF7, UTF16, UTF32 ,ASCII) e si utilizzano come tipi di codifica dei caratteri 
-                                         # ci sono delle differenze tra l'utilizzo dei diversi encoding(come per esempio l'utilizzo di bit per la scrittura dei caratteri o i caratteri contenuti)
+reader = print(open("Lezione15/esempio.txt", encoding="utf-8"))      # ci sono diversi encoding(UTF8, UTF7, UTF16, UTF32 ,ASCII) e si utilizzano come tipi di codifica dei caratteri 
+                                                                     # ci sono delle differenze tra l'utilizzo dei diversi encoding(come per esempio l'utilizzo di bit per la scrittura dei caratteri o i caratteri contenuti)
 
+with open("Lezione15/esempio.txt") as reader:
+    print(reader)
 
 try:
 
