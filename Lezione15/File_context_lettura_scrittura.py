5fff4a5c73758df5ee4d28dca156b762a75a67e5
GimmiPrud <prudenzig@gmail.com>
2024-06-12 10:42:05 +0200

    ...

--- Lezione15/File_context_lettura_scrittura.py
+++ Lezione15/File_context_lettura_scrittura.py
@@ -1,4 +1,6 @@
 
 # Giammarco Prudenzi 
 
-print(open("esempio.txt")) 
+print(open("esempio.txt"))      # ci sono diversi encoding(UTF8, UTF7, UTF16, UTF32 ,ASCII) e si utilizzano come tipi di codifica dei caratteri 
+                                # ci sono delle differenze tra l'utilizzo dei diversi encoding 
+
