d263d9c9c837c8785f84d49ccb1a1e11059538f6
GimmiPrud <prudenzig@gmail.com>
2024-06-25 15:52:07 +0200

    Lezione 18 esercizi classi interactiove calculator

---
+++ Lezione18/interactive_calculator.py
@@ -0,0 +1,18 @@
+
+# Giammarco Prudenzi
+
+# An interactive calculator:
+'''
+It is required to develop an interactive calculator with at least 10 test cases using UnitTest (possibly) all execution paths!
+User input is assumed to be a formula that consists of a number, an operator (at least and ),
+and another number, separated by white space (e.g. ). Split user input using str.split(), and check whether the resulting is valid:+-1 + 1list
+ If the input does not consist of 3 elements, raise a , which is a custom .FormulaErrorException
+Try to convert the first and third inputs to a (like so: ). 
+Catch any that occurs, and instead raise a .floatfloat_value = float(str_value)ValueErrorFormulaError
+If the second input is not or , again raise a .'+''-'FormulaError
+If the input is valid, perform the calculation and print out the result.
+The user is then prompted to provide new input, and so on, until the user enters .quit
+'''
+# Svolgimento:
+
+
