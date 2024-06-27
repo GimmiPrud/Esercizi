f9938e7916015517634ffbb5f1d2312bece95ac2
GimmiPrud <prudenzig@gmail.c>
2024-06-27 12:20:18 +0200

    ..

--- Lezione20/funzioni_e_decorator_context.py
+++ Lezione20/funzioni_e_decorator_context.py
@@ -111,3 +111,25 @@ print(next(prova_generatore))
 print(next(prova_generatore))
 print(next(prova_generatore))
 print(next(prova_generatore))
+
+# ES.
+
+from contextlib import contextmanager
+
+@contextmanager
+def context_manager_decorator(*args):
+    import time
+
+    start_time: float = time.time()
+
+    yield
+
+    end_time: float = time.time()
+    elapsed_time: float = end_time - start_time
+
+    print(f"{elapsed_time=}")  # in questo modo viene richiamata comunque la variabile 
+
+@ context_manager_decorator
+def area_cerchio(raggio: float):
+    
+    return raggio * raggio * 3.14
