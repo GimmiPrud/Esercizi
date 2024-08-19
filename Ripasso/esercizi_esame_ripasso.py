a20f9e465e71c972471382373f5c8424d6a048f7
GimmiPrud <prudenzig@gmail.com>
2024-08-19 19:32:38 +0200

    ...

--- Ripasso/esercizi_esame_ripasso.py
+++ Ripasso/esercizi_esame_ripasso.py
@@ -567,6 +567,7 @@ if False:
 
         def return_book(self):
             self.is_borrowed = False
+            
     class Member:
         def __init__(self, member_id: str, name: str):
             self.member_id = member_id
@@ -580,6 +581,7 @@ if False:
         def return_book(self, book: Book):
             if book.book_id in self.borrowed_books:
                 self.borrowed_books.remove(book)
+                
     class Library:
         def __init__(self):
             self.books: dict[str, Book] = {}
@@ -652,6 +654,133 @@ if False:
             return "Accesso negato"
 
 
+
+# Esercizio esame python teorico:
+'''
+Progettare un semplice sistema di gestione di studenti e corsi con i seguenti requisiti:
+1. Classe Student:
+Attributi:
+student_id: str - identificatore univoco per lo studente.
+courses: list[str] - la lista dei corsi ai quali lo studente è iscritto.
+Metodi:
+enroll(course: str)-aggiunge il corso specificato alla lista dei corsi dello studente oppure stampa il messaggio "Lo studente è già iscritto al corso {course}."
+get_courses(): restituisce la lista dei corsi ai quali lo studente è iscritto.
+Classe School:
+Attributi:
+students: dict[str, Student] - un dizionario per memorizzare gli studenti, in cui la chiave è una stringa ID mentre il valore è un oggetto di tipo Studente.
+Metodi:
+create_student(student_id: str): crea un nuovo studente con l'ID specificato e una lista di corsi vuota oppure stampa il messaggio "Lo studente con ID {student_id} esiste già."
+enroll_student(student_id: str, course: str): se lo studente esiste viene iscritto al corso specificato,
+altrimenti  stampa il messaggio "Studente non trovato."
+get_student_courses(student_id: str): se lo studente esiste restituisce la lista dei corsi dello studente con l'ID specificato,
+altrimenti ritorna il messaggio "Studente non trovato."
+get_stundent_list(): Ritorna una lista di tutte le chiavi all'interno del dizionario students.
+search_by_course(self, course: str): Trova e restituisce tutti gli ID degli studenti iscritti ad un determinato corso. 
+Restituisce una lista di tutte le chiavi all'interno del dizionario students che hanno il corso specificato tra i valori oppure il messaggio di errore "Nessuno studente è iscritto al corso {course}." 
+se non ci sono studenti che frequentano il corso specificato.
+'''
+class Student:
+    def __init__(self, student_id: str):
+        self.student_id = student_id
+        self.courses: list[str] = []
+
+    def enroll(self, course: str):
+        if course not in self.courses:
+            self.courses.append(course)
+        else:
+            print( f"Lo studente è già iscritto al corso {course}.")
+    
+    def get_courses(self):
+        return self.courses
+
+class School:
+    def __init__(self):
+        self.students:dict[str, Student] = {}
+
+    def create_student(self, student_id: str):
+        if student_id not in self.students.keys():
+            student: Student = Student(student_id)
+            self.students[student_id] = student
+        else:
+            print(f"Lo studente con ID {student_id} esiste già.")
+
+
+    def  enroll_student(self, student_id: str, course: str):   
+        if student_id in self.students.keys():
+            self.students[student_id].enroll(course)
+        else:
+            print(f"Studente non trovato.")
+
+    
+    def get_student_courses(self, student_id: str):
+        if student_id in self.students.keys():
+            self.students[student_id].get_courses()
+            return self.students[student_id].courses
+        else:
+            return f"Studente non trovato."
+
+
+    def get_student_list(self):
+        student_list = []
+        for i in self.students.keys():
+            student_list.append(i)
+        return student_list
+
+
+    def search_by_course(self, course: str):
+        list_st = []
+        for i in self.students.keys():
+            if course in self.students[i].courses:
+                list_st.append(i)
+
+        if len(list_st) != 0:
+            return list_st
+        else:
+            return f"Nessuno studente è iscritto al corso {course}."
+	
+
+
+# Creazione di una nuova scuola
+scuola = School()
+
+# Creazione di nuovi studenti
+scuola.create_student("1001")
+scuola.create_student("1002")
+scuola.create_student("1003")
+
+# Iscrizione degli studenti ai corsi
+scuola.enroll_student("1001", "Matematica")
+scuola.enroll_student("1002", "Fisica")
+scuola.enroll_student("1003", "Chimica")
+
+# Verifica dei corsi degli studenti        # result:
+print(scuola.get_student_courses("1001"))  # Output atteso: ['Matematica']
+print(scuola.get_student_courses("1002"))  # Output atteso: ['Fisica']
+print(scuola.get_student_courses("1003"))  # Output atteso: ['Chimica']
+
+
+# Creazione di una nuova scuola
+scuola = School()
+
+# Creazione di nuovi studenti
+scuola.create_student("1001")
+
+# Iscrizione degli studenti ai corsi
+scuola.enroll_student("1001", "Matematica")
+scuola.enroll_student("1001", "Matematica")
+
+# Tentativo di iscrizione di un corso per uno studente non esistente
+scuola.enroll_student("1004", "Fisica")
+
+# Verifica dei corsi degli studenti
+print(scuola.get_student_courses("1001"))
+print(scuola.get_student_courses("1004"))
+
+# result:
+# Lo studente è già iscritto al corso Matematica.
+# Studente non trovato.
+# ['Matematica']
+# Studente non trovato.
 
         
 
